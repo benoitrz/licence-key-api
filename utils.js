82d29fb8e008eed2fe5f844e7bb99a091d564758
Benoit Trzpit <benoit.trz@gmail.com>
2021-02-14 17:03:05 +0000

    Refactor formatString function to one-liner

--- utils.js
+++ utils.js
@@ -1,10 +1,7 @@
 const endpointPath = '/licence';
 const port = 3000;
 const authorizationSecret = 'arm_api_thursday';
-
-const formatString = (str) => {
-    return str.replace(/\s/g,'').toLowerCase();
-};
+const formatString = (str) => str.replace(/\s/g,'').toLowerCase();
 
 module.exports = {
     endpointPath, 
